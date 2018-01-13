c152b43831101120e233b80089a22823e7c8e9ae
Claire Pate <clairepate@Claires-Air.T-mobile.com>
2018-01-13 15:44:26 -0500

    set up

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Burger');
+INSERT INTO burgers (burger_name) VALUES ('Breakfest Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
