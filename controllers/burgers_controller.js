0d16caad9f8305f4cf3a58d85ed23ced8d3ed20b
Claire Pate <clairepate@Claires-Air.attlocal.net>
2018-01-19 18:58:35 -0500

    gitignore

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,57 +7,122 @@ var router = express.Router();
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
 
+
+router.get('/', function(req, res) {
+    res.redirect('/burgers');
+});
+
+//Get route to pull all burgers
+router.get('/burgers', function(req, res) {
+    burger.all(function(burgerData) {
+        res.render('index', { burger_data: burgerData });
+    });
+});
+
+//Post route to create a new burger with entered params
+router.post('/burgers/create', function(req, res) {
+    burger.create(req.body.burger_name, function(result) {
+        console.log(result);
+        res.redirect('/');
+    });
+});
+
+//Put route to update a burger
+router.put('/burgers/:id', function(req, res) {
+    burger.update(req.params.id, function(result) {
+        console.log(result);
+
+        res.sendStatus(200);
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
-});
+// router.get("/", function(req, res) {
+//   burger.all(function(data) {
+//     var hbsObject = {
+//       burgers: data
+//     };
+//     console.log(hbsObject);
+//     res.render("index", hbsObject);
+//   });
+// });
 
-router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "burger_name", "devoured"
-  ], [
-    req.body.burger_name, req.body.devoured
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
-});
+// router.post("/api/burgers", function(req, res) {
+//   burger.create([
+//     "burger_name", "devoured"
+//   ], [
+//     req.body.burger_name, req.body.devoured
+//   ], function(result) {
+//     // Send back the ID of the new quote
+//     res.json({ id: result.insertId });
+//   });
+// });
 
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+// router.put("/api/burgers/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
+//   console.log("condition", condition);
 
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
+//   burger.update({
+//     devoured: req.body.devoured
+//   }, condition, function(result) {
+//     if (result.changedRows == 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
 
-router.delete("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+// router.delete("/api/burgers/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
 
-  burger.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
+//   burger.delete(condition, function(result) {
+//     if (result.affectedRows == 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
 
 // Export routes for server.js to use.
 module.exports = router;
